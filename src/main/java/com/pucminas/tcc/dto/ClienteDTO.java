eabe1ce284236304a6721a86edfbae01c9733817
Antonio-QA <thonysiq@gmail.com>
2022-03-30 14:03:08 -0300

    Implementação de operações CRUD - Cliente

---
+++ src/main/java/com/pucminas/tcc/dto/ClienteDTO.java
@@ -0,0 +1,60 @@
+package com.pucminas.tcc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.pucminas.tcc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "Campo Obrigatório")
+	@Length(min = 5, max = 250, message = "O campo deve conter entre 5 e 250 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Email(message = "E-mail inválido")
+	private String email;
+	
+	
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = getEmail();
+	}
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
--- src/main/java/com/pucminas/tcc/services/ClienteService.java
+++ src/main/java/com/pucminas/tcc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.pucminas.tcc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pucminas.tcc.domain.Cliente;
+import com.pucminas.tcc.dto.ClienteDTO;
 import com.pucminas.tcc.repositories.ClienteRepository;
+import com.pucminas.tcc.services.exceptions.DataIntegrityException;
 import com.pucminas.tcc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,12 +21,47 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())
 		);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir clientes que possuam produtos vinculados");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 }
